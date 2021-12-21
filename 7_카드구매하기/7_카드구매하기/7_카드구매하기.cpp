81a562cb54c91a1451bb3816f45f723f16639e8f
Isak Song <thddltkr10@naver.com>
2021-12-21 16:54:39 +0900

    git push -u origin main

---
+++ 7_카드구매하기/7_카드구매하기/7_카드구매하기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define endl "\n"
+#define Max 1001
+using namespace std;
+
+int n;
+int arr[Max];
+int dp[Max];
+
+void input() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+}
+int main()
+{
+    input();
+    dp[1] = arr[1];
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (dp[i] < dp[i - j] + arr[j]) {
+                dp[i] = dp[i - j] + arr[j];
+            }
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
+
---
+++ 8_포도주 시식/8_포도주 시식/8_포도주 시식.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstring>
+
+#define endl "\n"
+#define Max 10001
+using namespace std;
+
+int n;
+int arr[Max];
+int dp[Max];
+
+void input()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main()
+{
+    input();
+    if (n == 1) {
+        cout << arr[1];
+    }
+    else if (n == 2) {
+        cout << arr[1] + arr[2];
+    }
+    else if (n == 3) {
+        cout << std::max(std::max(arr[1] + arr[3], arr[2] + arr[3]), arr[1] + arr[2]);
+    }
+    else {
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+        dp[3] = std::max(std::max(arr[1] + arr[3], arr[2] + arr[3]), arr[1] + arr[2]);
+        for (int i = 4; i <= n; i++) {
+            dp[i] = std::max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+            dp[i] = std::max(dp[i], dp[i - 1]);
+        }
+        cout << dp[n];
+    }
+    return 0;
+}
+
+
